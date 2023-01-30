5e4eaef9db9399dd80e3f9f4b10a2084a6f962f9
Brandon Sedman <brandon.sedman@gmail.com>
2023-01-30 13:11:42 -0500

    small change - fixed duplicate product DbSet in CaaContext

--- CAA_TestApp/Data/CaaContext.cs
+++ CAA_TestApp/Data/CaaContext.cs
@@ -15,7 +15,6 @@ namespace CAA_TestApp.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Location> Locations { get; set; }
         public DbSet<Event> Events { get; set; }
-        public DbSet<Product> products { get; set; }
         public DbSet<ItemPhoto> ItemsPhotos { get; set; }
         public DbSet<ItemThumbnail> ItemsThumbnails { get; set; }
 
